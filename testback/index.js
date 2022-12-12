0e84d65a351baa820b7db99f6b9f13f3982cb177
suman2013 <suman.sutar2013@gmail.com>
2022-12-12 10:44:28 -0600

    sample routes added

--- testback/index.js
+++ testback/index.js
@@ -1,3 +1,25 @@
 const express = require("express");
-
 const app = express();
+const port = 3000;
+
+//Practicing sample routes
+
+app.get("/", (req, res) => {
+	return res.send("Home Page");
+});
+
+app.get("/login", (req, res) => {
+	return res.send("You are visiting login route");
+});
+
+app.get("/logout", (req, res) => {
+	return res.send("Hey, You are signed out successfully");
+});
+
+app.get("/user", (req, res) => {
+	res.send("Suman is using Githun");
+});
+
+app.listen(port, () => {
+	console.log("app is running");
+});
