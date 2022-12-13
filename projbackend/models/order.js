d7f8758e9a01709f80a658269fefbd991729f211
suman2013 <suman.sutar2013@gmail.com>
2022-12-13 10:27:47 -0600

    order schema added

---
+++ projbackend/models/order.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const {ObjectId} = mongoose.Schema;
+
+//Productcart Schema
+const productCartScehma = new mongoose.Schema({
+  product:{
+    type:ObjectId,
+    ref:"Product"
+  },
+  name:String,
+  count:Number,
+  price:Number,
+
+})
+
+const ProductCart = mongoose.model("ProductCart", productCartScehma);
+
+//Order Schema
+const orderSchema = new mongoose.Schema({
+  products:[productCartScehma],
+  transaction_id:{},
+  amount:{type:Number},
+  updated:Date,
+  address:String,
+  user:{
+    type:ObjectId,
+    ref:"User"
+  },
+  {timestamps:true}
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = {ProductCart, Order};
