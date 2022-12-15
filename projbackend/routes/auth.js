50573d441ead41008035124cd855d0cc07bf5001
suman2013 <suman.sutar2013@gmail.com>
2022-12-15 13:09:45 -0600

    signin completed

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router();
-
+const {check, validationResult} = require("express-validator");
 //Importing functions
-const {signup, signout} = require("../controllers/auth");
+const {signup, signin, signout} = require("../controllers/auth");
 
 //Routers
-router.post("/signup", signup);
+//Sign up
+router.post("/signup", [
+  check("name").isLength({min: 3}).withMessage("Name should have atleast 3 char"),
+  check("email").isEmail().withMessage("Email is required"),
+  check("password").isLength({min:5}).withMessage("Password should have atleast 5 char")
+], signup);
+
+//Sign in
+router.post("/signin", [
+  check("email").isEmail().withMessage("Valid email is required"),
+  check("password").isLength({min: 1}).withMessage("Password is required")
+], signin);
+
+//Sign out
 router.get("/signout", signout);
 
 module.exports = router;
